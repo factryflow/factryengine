c01144d6fe046a83e36a9a24c772bfe62b71b2c1
Jacob Østergaard Nielsen <jaoe@oestergaard-as.dk>
2023-04-24 23:38:58 +0200

    Made OP changes

--- Scheduler.py
+++ Scheduler.py
@@ -2,19 +2,18 @@ from dataclasses import dataclass, field
 from typing import List, Tuple, Optional
 from ortools.sat.python import cp_model
 
-
 @dataclass
 class Resource:
-    resource_id: int
+    id: int
     availability_slots: List = field(default_factory=list)
 
     def __eq__(self, other):
         if isinstance(other, Resource):
-            return self.resource_id == other.resource_id
+            return self.id == other.id
         return NotImplemented
     
     def __hash__(self):
-        return hash(self.resource_id)
+        return hash(self.id)
 
 @dataclass
 class ResourceGroup:
@@ -31,7 +30,7 @@ class ResourceGroup:
     
 @dataclass
 class Task:
-    task_id: int
+    id: int
     duration: int
     priority: int
     resource_group: ResourceGroup
@@ -39,148 +38,171 @@ class Task:
 
     def __eq__(self, other):
         if isinstance(other, Task):
-            return self.task_id == other.task_id
+            return self.id == other.id
         return NotImplemented
 
     def __hash__(self):
-        return hash(self.task_id)
-
-def Scheduler(tasks, horizon):
-
-    resource_groups = set(task.resource_group for task in tasks)
-    resources = set(resource for resource_group in resource_groups for resource in resource_group.resources) 
+        return hash(self.id)
     
-    availability_slots = [
-        {"slot_id" : 0, "start":0, "end":4}, # duration 1
-        {"slot_id" : 1, "start":5, "end":10}, # duration 2
-        {"slot_id" : 2, "start":15, "end":20}, # duration 3
-        {"slot_id" : 3, "start":22, "end":50} # duration 4
-    ]
+    def get_resources(self):
+        return self.resource_group.resources
 
-    model = cp_model.CpModel()
+class Scheduler:
+    def __init__(self, tasks, horizon):
+        self.tasks = tasks
+        self.horizon = horizon
+        self.resource_bools = None
+        self.subtask_vars = None
 
-    # subtaks 
-    vars = {task.task_id: [] for task in tasks}
+    def schedule(self):
+        tasks = self.tasks
+        horizon = self.horizon
 
-    # Create subtasks for each availability slot
-    for task in tasks:
-        task_id = task.task_id
-        task_duration = task.duration
-        subtask_durations = []
-        for slot in availability_slots:
+        resource_groups = set(task.resource_group for task in tasks)
+        resources = set(resource for resource_group in resource_groups for resource in resource_group.resources) 
+        self.resource_groups = resource_groups
+        self.resources = resources
 
-            # create variables
-            start = model.NewIntVar(slot["start"], slot["end"], f'start_{slot["start"]}')
-            end = model.NewIntVar(slot["start"], slot["end"], f'end_{slot["end"]}')
-            duration = model.NewIntVar(0, task_duration, f'duration_{slot["slot_id"]}')
-            subtask_durations.append(duration)
-            interval = model.NewIntervalVar(start, duration, end, f'interval_{task_id, slot["slot_id"]}')
+        availability_slots = [
+            {"slot_id" : 0, "start":0, "end":4}, # duration 1
+            {"slot_id" : 1, "start":5, "end":10}, # duration 2
+            {"slot_id" : 2, "start":15, "end":20}, # duration 3
+            {"slot_id" : 3, "start":22, "end":50} # duration 4
+        ]
 
-            # Create a BoolVar to check if slot is task start
-            is_task_start = model.NewBoolVar(f'task_start_{task_id}_{slot["slot_id"]}')
-            model.Add(sum(subtask_durations) == duration).OnlyEnforceIf((is_task_start))
-            model.Add(sum(subtask_durations) != duration).OnlyEnforceIf(is_task_start.Not())
+        model = cp_model.CpModel()
 
-            # Create a BoolVar to check if task has started
-            is_in_progress = model.NewBoolVar(f'task_started_{task_id}_{slot["slot_id"]}')
-            model.Add(sum(subtask_durations) > 0).OnlyEnforceIf(is_in_progress)
-            model.Add(sum(subtask_durations) == 0).OnlyEnforceIf(is_in_progress.Not())
+        # create resource bools
+        resource_bools = {}
+        for task in tasks:
+            for resource in task.get_resources():
+                resource_bools[task.id, resource.id] = model.NewBoolVar(f'x[{task.id},{resource.id}]')
+        
+        self.resource_bools = resource_bools
 
-            # Create a BoolVar to check if task has ended
-            is_task_end = model.NewBoolVar(f'task_ended_{task_id}_{slot["slot_id"]}')
-            model.Add(sum(subtask_durations) == task_duration).OnlyEnforceIf(is_task_end)
-            model.Add(sum(subtask_durations) != task_duration).OnlyEnforceIf(is_task_end.Not())
+        # constrain tasks to one resource
+        for task in tasks:
+            model.AddExactlyOne([resource_bools[task.id, resource.id] for resource in task.get_resources()])
 
-            # Ensure subtasks are continuous 
-            # if task is in progress it should fill the whole slot
-            model.Add(start == slot["start"]).OnlyEnforceIf((is_in_progress,is_task_end.Not(), is_task_start.Not()))
-            model.Add(end == slot["end"]).OnlyEnforceIf((is_in_progress,is_task_end.Not(), is_task_start.Not()))
+        # subtaks 
+        subtask_vars = {(task.id,resource.id): [] for task in tasks for resource in task.get_resources()}
 
-            # if task is starting but not ending
-            model.Add(end == slot["end"]).OnlyEnforceIf((is_task_start, is_task_end.Not()))
+        # Create subtasks for each availability slot
+        for task in tasks:
+            task.id = task.id
+            task_duration = task.duration
+            for resource in task.get_resources():
+                subtask_durations = []
+                for subtask_id, slot in enumerate(resource.availability_slots):
 
-            # if task is ending but not starting
-            model.Add(start == slot["start"]).OnlyEnforceIf((is_task_end,is_task_start.Not()))        
+                    # create variables
+                    start = model.NewIntVar(slot["start"], slot["end"], f'start_{slot["start"]}')
+                    end = model.NewIntVar(slot["start"], slot["end"], f'end_{slot["end"]}')
+                    duration = model.NewIntVar(0, task_duration, f'duration_{slot["slot_id"]}')
+                    subtask_durations.append(duration)
+                    interval = model.NewIntervalVar(start, duration, end, f'interval_{task.id, slot["slot_id"]}')
 
-            # Add the subtask to the list
-            vars[task_id].append({
-                "subtask_id" : slot["slot_id"],
-                "start" : start,
-                "duration" : duration,
-                "end" : end,
-                "interval" : interval,
-                "slot_id": slot["slot_id"],
-                "is_task_start" : is_task_start,
-                "is_in_progress" : is_in_progress,
-                "is_task_end" : is_task_end
-            })
+                    # Create a BoolVar to check if slot is task start
+                    is_task_start = model.NewBoolVar(f'task_start_{task.id}_{slot["slot_id"]}')
+                    model.Add(sum(subtask_durations) == duration).OnlyEnforceIf((is_task_start))
+                    model.Add(sum(subtask_durations) != duration).OnlyEnforceIf(is_task_start.Not())
 
-        # Add constraint to enforce the sum of durations of subtasks to be equal to task_duration
-        model.Add(sum(subtask_vars["duration"] for subtask_vars in vars[task_id]) == task_duration)
+                    # Create a BoolVar to check if task has ended
+                    is_task_end = model.NewBoolVar(f'task_ended_{task.id}_{slot["slot_id"]}')
+                    model.Add(sum(subtask_durations) == task_duration).OnlyEnforceIf(is_task_end)
+                    model.Add(sum(subtask_durations) != task_duration).OnlyEnforceIf(is_task_end.Not())
 
-    # unpack vars
-    all_intervals = []
-    all_ends = []
+                    # Create a BoolVar if duration is 0
+                    duration_is_zero = model.NewBoolVar(f'duration_is_zero_{task.id}_{slot["slot_id"]}')
+                    model.Add(duration == 0).OnlyEnforceIf(duration_is_zero)
+                    model.Add(duration != 0).OnlyEnforceIf(duration_is_zero.Not())
 
-    for task_id, subtasks in vars.items():
-        for subtask in subtasks:
-            all_intervals.append(subtask["interval"])
-            all_ends.append(subtask["end"])
+                    # Ensure is_in_progress is true when subtask_durations is between > 0 and 10
+                    is_in_progress = model.NewBoolVar(f'task_started_{task.id}_{slot["slot_id"]}')
+        
+                    model.Add(sum(subtask_durations) == task_duration).OnlyEnforceIf(is_in_progress)
+                    model.Add(sum(subtask_durations) != task_duration).OnlyEnforceIf(is_in_progress.Not())
+                    
+                    # Ensure subtasks are continuous 
+                    # if task is in progress it should fill the whole slot
+                    model.Add(start == slot["start"]).OnlyEnforceIf((is_in_progress,is_task_end.Not(), is_task_start.Not()))
+                    model.Add(end == slot["end"]).OnlyEnforceIf((is_in_progress,is_task_end.Not(), is_task_start.Not()))
 
+                    # if task is starting but not ending
+                    model.Add(end == slot["end"]).OnlyEnforceIf((is_task_start, is_task_end.Not()))
 
-    # model.AddNoOverlap(all_intervals)
+                    # if task is ending but not starting
+                    model.Add(start == slot["start"]).OnlyEnforceIf((is_task_end,is_task_start.Not()))        
 
-    # resource implementation
-    x = {}
-    for task in tasks:
-        for resource in task.resource_group.resources:
-            x[resource, task.task_id] = model.NewBoolVar(f'x[{resource},{task.task_id}]')
+                    # Add the subtask to the list
+                    subtask_vars[task.id,resource.id].append({
+                        "subtask_id" : subtask_id,
+                        "slot_id": slot["slot_id"],
+                        "start" : start,
+                        "duration" : duration,
+                        "end" : end,
+                        "interval" : interval,
+                        "is_task_start" : is_task_start,
+                        "is_in_progress" : is_in_progress,
+                        "is_task_end" : is_task_end,
+                        "duration_is_zero" : duration_is_zero
+                    })
 
-    for task in tasks:
-        model.AddExactlyOne([x[resource, task.task_id] for resource in task.resource_group.resources])
+                # Add constraint to enforce the sum of durations of subtasks to be equal to task_duration
+                model.Add(sum(subtask_durations) == task_duration).OnlyEnforceIf(resource_bools[task.id,resource.id])
+                model.Add(sum(subtask_durations) == 0).OnlyEnforceIf(resource_bools[task.id,resource.id].Not())
 
-    # Add NoOverlap constraint for each resource.
-    resource_intervals = {resource: [] for resource in resources}
-    for task in tasks:
-        for subtask in vars[task.task_id]:
-            for resource in task.resource_group.resources:
+        self.subtask_vars = subtask_vars
+
+        # Add NoOverlap constraint for each resource.
+        resource_intervals = {resource.id: [] for resource in resources}
+        for (task_id, resource_id), subtask_list in subtask_vars.items():
+            for subtask in subtask_list:
                 optional_interval = model.NewOptionalIntervalVar(
                     start = subtask["start"], 
                     size = subtask["duration"], 
                     end = subtask["end"], 
-                    is_present = x[resource, task.task_id],
-                    name = f'resource_interval_{resource}_{task.task_id, subtask["subtask_id"]}'
+                    is_present = resource_bools[task_id, resource_id],
+                    name = f'resource_interval_{resource}_{task_id, subtask["subtask_id"]}'
                     )
-                resource_intervals[resource].append(optional_interval)
+                resource_intervals[resource_id].append(optional_interval)
 
-    for resource in resources:
-        model.AddNoOverlap(resource_intervals[resource])
+        for resource in resources:
+            model.AddNoOverlap(resource_intervals[resource.id])
 
-    # object var to reduce makespan
+        # object var to reduce makespan
+        # only consider ends with duration > 0
+        optional_ends = []
+        for (task_id, resource_id), subtask_list in subtask_vars.items():
+            for subtask in subtask_list:
+                end_var = model.NewIntVar(0, horizon, f'end_var_{task_id}_{resource_id}_{subtask["subtask_id"]}')
+                model.AddElement(subtask["duration_is_zero"], [subtask["end"],0], end_var)
+                optional_ends.append(end_var)
 
-    obj_var = model.NewIntVar(0, horizon, 'makespan')
-    model.AddMaxEquality(obj_var, all_ends)
-    model.Minimize(obj_var)
+        obj_var = model.NewIntVar(0, horizon, 'makespan')
+        model.AddMaxEquality(obj_var, optional_ends)
+        model.Minimize(obj_var)
+        
+        # Create a solver and solve the model
+        solver = cp_model.CpSolver()
+        status = solver.Solve(model)
 
-    # Create a solver and solve the model
-    solver = cp_model.CpSolver()
-    status = solver.Solve(model)
+        # Check the solver status and print the solution
+        if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+            print(f"Makespan = {solver.ObjectiveValue()}")
+            for task in tasks:
+                for resource in task.get_resources():
+                    if solver.BooleanValue(resource_bools[task.id, resource.id]):
 
-    # Check the solver status and print the solution
-    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-        print(f"Makespan = {solver.ObjectiveValue()}")
-        for task_id, subtasks in vars.items():
-            for resource in task.resource_group.resources:
-                if solver.BooleanValue(x[resource, task_id]):
-                    print(f"Assigned to resource {resource.resource_id}")
-            for subtask in subtasks:
-                if solver.Value(subtask['duration']) > 0:
-                    print(
-                        f"Task {(task_id, subtask['subtask_id'])}: "
-                        f"starts: {solver.Value(subtask['start'])}, "
-                        f"end: {solver.Value(subtask['end'])}, "
-                        f"duration: {solver.Value(subtask['duration'])}, "
-                        f"bools: {[solver.Value(subtask[field]) for field in ['is_task_start', 'is_in_progress', 'is_task_end']]}"
-                        )
-    else:
-        print("No solution found.")
+                        print(f"Assigned to resource {resource.id}")
+                        for subtask in subtask_vars[task.id, resource.id]:
+                            # if solver.Value(subtask['duration']) > 0:
+                                print(
+                                    f"Task {(task.id, subtask['subtask_id'])}: "
+                                    f"starts: {solver.Value(subtask['start'])}, "
+                                    f"end: {solver.Value(subtask['end'])}, "
+                                    f"duration: {solver.Value(subtask['duration'])}, "
+                                    f"bools: {[solver.Value(subtask[field]) for field in ['is_task_start', 'is_in_progress', 'is_task_end', 'duration_is_zero']]}"
+                                    )
+        else:
+            print("No solution found.")
