d3275b609db988a00404043d2ecb8da53101fb99
Jacob Østergaard Nielsen <jaoe@oestergaard-as.dk>
2023-04-27 16:35:20 +0200

    progress on heuristic solver

--- PyPlanPro/scheduler/heuristic_solver.py
+++ PyPlanPro/scheduler/heuristic_solver.py
@@ -7,8 +7,31 @@ class HeuristicSolver():
 
     def solve(self):
         task_ids_ordered = self._get_task_order(self.tasks)
-        print(task_ids_ordered)
-    
+        task_dict = {task.id: task for task in self.tasks}
+        task_vars = {task.id: {} for task in self.tasks}
+        resource_intervals = self._get_resource_intervals(self.resouces)
+
+        for task_id in task_ids_ordered:
+            task = task_dict[task_id]
+            # get max_start_time from max predecessors end
+            max_start_time = max((task_vars[pred.id].get('task_end',0) for pred in task.predecessors), default=0)
+            
+            # find the resource who completes the task first
+            fastest_resource = self._get_fastest_resource(task,resource_intervals, max_start_time)
+            if fastest_resource is None:
+                print(f"No resource could be found for task_id = {task.id}")
+                continue
+            # update task_vars with assigned resource, task_start and task_end
+            
+            task_values = {
+                "assigned_resource": fastest_resource["resource"], 
+                "task_start": fastest_resource["start_time"],
+                "task_end" : fastest_resource["end_time"]
+                }
+            
+            task_vars[task.id].update(task_values)
+
+        return task_vars
     def _get_task_order(self, tasks):
         """
         Given a dictionary of tasks with durations, predecessors, and priorities, this function returns a list of tasks
@@ -24,7 +47,7 @@ class HeuristicSolver():
         for task  in tasks:
             G.add_node(task.id, duration=task.duration, priority=task.priority)
             for predecessor in task.predecessors:
-                G.add_edge(predecessor, task.id)
+                G.add_edge(predecessor.id, task.id)
 
         # Compute the longest path for each node using a topological sort
         longest_path = {task.id: 0 for task in tasks}
@@ -58,6 +81,26 @@ class HeuristicSolver():
 
         return result
 
+    def _get_fastest_resource(self, task, resource_intervals, max_start_time):
+        """Returns the resource with the earliest end time for a task."""
+        resource_start_ends = []
+        for resource in task.get_resources():
+            resource_interval = resource_intervals[resource.id]
+            for interval_index, interval in enumerate(resource_interval):
+                if interval["duration"] < task.duration:
+                    continue
+                start, end = self._get_task_earliest_start_end(interval["slots"], task.duration, max_start_time)
+                if start is not None:
+                    resource_start_ends.append({
+                        "resource": resource,
+                        "interval_index": interval_index,
+                        "start_time": start,
+                        "end_time": end
+                    })
+        if not resource_start_ends:
+            return None
+        return min(resource_start_ends, key=lambda x: x['end_time']) 
+
     def _update_resource_intervals(self, resource_intervals, resource_id, interval_index, task_start, task_end):
         interval = resource_intervals(resource_id).pop(interval_index)
         new_intervals_slots = self._trim_and_split_interval(interval["slots"], task_start, task_end)
@@ -84,7 +127,7 @@ class HeuristicSolver():
                 return (task_start,task_end) 
             
             remaining_duration -= slot_duration
-        return None
+        return None, None
 
     def _trim_and_split_interval(self, slots, task_start, task_end):
         interval1 = []
@@ -104,6 +147,9 @@ class HeuristicSolver():
                 interval2.append((task_end, end))
         return interval1, interval2
 
+    def _get_resource_intervals(self, resources):
+        return {resource.id : [self._create_resource_interval(resource.availability_slots)] for resource in resources}
+
     def _create_resource_interval(self, slots):
         return {
             "slots":slots,
@@ -116,5 +162,6 @@ class HeuristicSolver():
     
     def _get_interval_start_end(self, slots):
         return (slots[0][0], slots[-1][1])
+    
 
 
