49606b9ef47ca0f3e2cd66a440d1aecc58be2b9f
Jacob Østergaard Nielsen <jaoe@oestergaard-as.dk>
2023-05-01 16:42:14 +0200

    update version to 0.0.5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read_requirements(path):
 
 setup(
     name='pyplanpro',
-    version='0.0.4',
+    version='0.0.5',
     author='Jacob Østergaard Nielsen',
     author_email='jaoe@oestergaard-as.dk',
     description='Task scheduler for python',
