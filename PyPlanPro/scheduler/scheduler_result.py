c101d68162ac4fdfebd48330732395f0f2ae0429
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-05-25 19:10:07 +0200

    implemented plot_resource_plan

--- PyPlanPro/scheduler/scheduler_result.py
+++ PyPlanPro/scheduler/scheduler_result.py
@@ -1,6 +1,9 @@
 # scheduler_result.py
-
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 class SchedulerResult():
     def __init__(self, task_vars):
@@ -19,3 +22,40 @@ class SchedulerResult():
         if self.unscheduled_tasks: 
             summary += f"\nNo available resources found for task ids: {', '.join(map(str, self.unscheduled_tasks))}"
         return summary
+
+    def plot_resource_plan(self) -> None:
+        df = self.to_dataframe()
+            # Sort dataframe by assigned_resource_id and task_start to visualize in order
+        df = df.sort_values(['assigned_resource_id', 'task_start'])
+
+        # Create a color dictionary for each unique resource for distinction in the plot
+        resources = df['assigned_resource_id'].unique()
+        colors = sns.color_palette("deep", len(resources))  # Using seaborn "dark" color palette
+        color_dict = dict(zip(resources, colors))
+
+        # Set seaborn style
+        sns.set_style("whitegrid")
+
+        plt.figure(figsize=(12, 6))
+
+        for resource_id, group_df in df.groupby('assigned_resource_id'):
+            for task in group_df.itertuples():
+                plt.barh(resource_id, 
+                        left=task.task_start, 
+                        width=task.task_end - task.task_start, 
+                        color=color_dict[resource_id],
+                        edgecolor='black')
+                plt.text(x=(task.task_start + task.task_end)/2,  # x position, in the middle of task bar
+                        y=resource_id,  # y position, on the resource line
+                        s=task.task_id,  # text string, which is task_id here
+                        va='center',  # vertical alignment
+                        ha='center',  # horizontal alignment
+                        color='black',  # text color
+                        fontsize=10)  # font size
+
+        plt.xlabel('Time')
+        plt.ylabel('Resource')
+        plt.title('Resource Plan')
+        plt.yticks(df['assigned_resource_id'].unique())
+        plt.tight_layout()  # adjusts subplot params so that the subplot(s) fits into the figure area.
+        plt.show()
