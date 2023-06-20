cbd4d7d816b686a89712852dea5884db8892bbcb
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-06-20 14:13:12 +0200

    updated get resources

--- src/pyplanpro/scheduler/core.py
+++ src/pyplanpro/scheduler/core.py
@@ -1,15 +1,19 @@
 from .heuristic_solver.main import HeuristicSolver
 from .scheduler_result import SchedulerResult
 
-class Scheduler():
+
+class Scheduler:
     def __init__(self, tasks):
         self.tasks = tasks
-        self.resources = set([resource for task in tasks for constraint in task.constraints for resource in constraint.get_resources()])
-    
+        self.resources = set(
+            [resource for task in tasks for resource in task.get_resources()]
+        )
+
     def schedule(self):
         heuristic_solver = HeuristicSolver(self.tasks, self.resources)
         result = heuristic_solver.solve()
 
         scheduler_result = SchedulerResult(result)
         print(scheduler_result.summary())
+        return scheduler_result
         return scheduler_result
