9820ed2e18e776f270f3bbbd44e5380e139ffc27
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-06-27 11:12:21 +0200

    simplified models

--- src/pyplanpro/models/task.py
+++ src/pyplanpro/models/task.py
@@ -9,7 +9,6 @@ class Task(BaseModel):
     id: int
     duration: int
     priority: int
-    # resource_constraints: list[Union[ResourceGroupConstraint, SingleResourceConstraint]]
     resources: list[Resource]
     resource_count: int = 1
     predecessors: Optional[list["Task"]] = []
@@ -17,22 +16,3 @@ class Task(BaseModel):
 
     def get_resources(self):
         return self.resources
-
-    # def get_resource_options(self):
-    #     return [
-    #         resources
-    #         for constraint in self.resource_constraints
-    #         for resources in constraint.get_possible_resources()
-    #     ]
-
-    # def get_unique_resource_combinations(self):
-    #     all_combinations = set(
-    #         frozenset(combination)
-    #         for combination in product(*self.get_resource_options())
-    #     )
-    #     unique_combinations = [
-    #         set(s)
-    #         for s in all_combinations
-    #         if len(s) == len(self.get_resource_options())
-    #     ]
-    #     return unique_combinations
