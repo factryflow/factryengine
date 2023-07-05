995b5bb9f1c42addd8f56df8f6aacc1c45fc49a8
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-07-05 13:50:55 +0200

    added the option for multiple resource teams

--- src/pyplanpro/models/task.py
+++ src/pyplanpro/models/task.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 from .resource import Resource
 
@@ -9,10 +9,22 @@ class Task(BaseModel):
     id: int
     duration: int
     priority: int
-    resources: list[Resource]
+    resources: list[list[Resource]]
     resource_count: int = 1
     predecessors: Optional[list["Task"]] = []
     predecessor_delay: int = 0
 
+    @validator("resources", pre=True)
+    def ensure_list(cls, v):
+        if not isinstance(v, list):  # if a single resource object is passed
+            return [[v]]  # make it a list of list
+        if (
+            isinstance(v, list) and len(v) > 0 and not isinstance(v[0], list)
+        ):  # if a list of resources is passed
+            return [v]  # make it a list of list
+        return v  # if a list of lists is passed, return as it is
+
     def get_resources(self):
-        return self.resources
+        return [
+            resource for resource_list in self.resources for resource in resource_list
+        ]
