a09cc58b99f32687d377a4ecab2d4475c503d4a8
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-07-11 12:40:49 +0200

    added versioneer

--- setup.py
+++ setup.py
@@ -1,4 +1,35 @@
-from setuptools import find_packages, setup
+import setuptools
 
-if __name__ == "__main__":
-    setup(packages=find_packages(where="src"))
+import versioneer
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="planbee",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="production / job shop / resource scheduler for Python",
+    author="Jacob Østergaard Nielsen",
+    author_email="jaoe@oestergaard-as.dk",
+    url="https://github.com/Yacobolo/PlanBee",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    python_requires=">=3.6",
+    install_requires=requirements,
+    extras_require={
+        "testing": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+        ],
+    },
+)
--- src/planbee/__init__.py
+++ src/planbee/__init__.py
@@ -1,3 +1,6 @@
 from .models.resource import Resource  # noqa: F401
 from .models.task import Task  # noqa: F401
 from .scheduler.core import Scheduler  # noqa: F401
+
+from . import _version
+__version__ = _version.get_versions()['version']
