04a8a4e7337c9d1bfbc9c37a37bcf4e0fff7db8b
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-07-25 16:26:22 +0200

    make batch_id private

--- src/planbee/models/task.py
+++ src/planbee/models/task.py
@@ -1,22 +1,29 @@
 from itertools import count
 from typing import Optional, Union
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, Field, PrivateAttr, validator
 
 from .resource import Resource
 
 
 class Task(BaseModel):
     id: Union[int, str]
-    duration: int
-    priority: int
+    duration: int = Field(..., gt=0)
+    priority: int = Field(..., gt=0)
     resources: list[set[Resource]]
     resource_count: Union[int, str] = 1
     predecessors: Optional[list["Task"]] = []
-    predecessor_delay: int = 0
-    batch_size: Optional[int] = None
-    quantity: Optional[int] = None
-    batch_id: Optional[int] = None
+    predecessor_delay: int = Field(0, gt=0)
+    batch_size: Optional[int] = Field(None, gt=0)
+    quantity: Optional[int] = Field(None, gt=0)
+    _batch_id: Optional[int] = PrivateAttr()
+
+    @property
+    def batch_id(self):
+        return self._batch_id
+
+    def set_batch_id(self, batch_id):
+        self._batch_id = batch_id
 
     @validator("resources", pre=True)
     def ensure_list(cls, v):
@@ -33,16 +40,6 @@ class Task(BaseModel):
             resource for resource_list in self.resources for resource in resource_list
         ]
 
-    def get_resource_group_count(self):
-        return len(self.resources)
-
-    def get_resource_group_indices(self) -> list[list[int]]:
-        """
-        returns a list of lists of indices of resources in each resource group
-        """
-        counter = count()
-        return [[next(counter) for _ in sublist] for sublist in self.resources]
-
     @validator("resource_count", always=True)
     def set_resource_count(cls, v, values):
         if isinstance(v, str) and v.lower() == "all":
@@ -55,3 +52,13 @@ class Task(BaseModel):
             return v
         else:
             raise ValueError("Invalid value for resource_count.")
+
+    def get_resource_group_count(self):
+        return len(self.resources)
+
+    def get_resource_group_indices(self) -> list[list[int]]:
+        """
+        returns a list of lists of indices of resources in each resource group
+        """
+        counter = count()
+        return [[next(counter) for _ in sublist] for sublist in self.resources]
