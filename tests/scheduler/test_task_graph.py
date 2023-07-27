1b789dece946f6de5299358789fabd4193703fd5
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-07-27 15:26:11 +0200

    added tests

--- tests/scheduler/test_task_graph.py
+++ tests/scheduler/test_task_graph.py
@@ -6,42 +6,48 @@ from planbee.scheduler.task_graph import TaskGraph
 
 
 @pytest.fixture
-def tasks():
+def tasks_dict() -> dict[str, Task]:
     task1 = Task(id=1, duration=2, priority=1, resources=[], predecessors=[])
     task2 = Task(id=2, duration=1, priority=2, resources=[], predecessors=[task1])
     task3 = Task(
         id=3, duration=3, priority=3, resources=[], predecessors=[task1, task2]
     )
-    return [task1, task2, task3]
+    tasks_dict = {
+        "1": task1,
+        "2": task2,
+        "3": task3,
+    }
+
+    return tasks_dict
 
 
-def test_create_task_graph(tasks):
-    task_graph = TaskGraph(tasks)
+def test_create_task_graph(tasks_dict):
+    task_graph = TaskGraph(tasks_dict)
 
     assert len(task_graph.graph) == 3
     assert len(task_graph.graph.edges) == 3
-    assert 1 in task_graph.graph
-    assert 2 in task_graph.graph
-    assert 3 in task_graph.graph
+    assert "1" in task_graph.graph
+    assert "2" in task_graph.graph
+    assert "3" in task_graph.graph
 
 
-def test_compute_longest_paths(tasks):
-    task_graph = TaskGraph(tasks)
+def test_compute_longest_paths(tasks_dict):
+    task_graph = TaskGraph(tasks_dict)
     longest_paths = task_graph._compute_longest_paths()
 
-    assert longest_paths == {1: 0, 2: 1, 3: 4}
+    assert longest_paths == {"1": 0, "2": 1, "3": 4}
 
 
-def test_custom_topological_sort(tasks):
-    task_graph = TaskGraph(tasks)
+def test_custom_topological_sort(tasks_dict):
+    task_graph = TaskGraph(tasks_dict)
     longest_paths = task_graph._compute_longest_paths()
     sorted_tasks = task_graph._custom_topological_sort(longest_paths)
 
-    assert sorted_tasks == [1, 2, 3]
+    assert sorted_tasks == ["1", "2", "3"]
 
 
-def test_get_task_order(tasks):
-    task_graph = TaskGraph(tasks)
+def test_get_task_order(tasks_dict):
+    task_graph = TaskGraph(tasks_dict)
     task_order = task_graph.get_task_order()
 
-    assert task_order == [1, 2, 3]
+    assert task_order == ["1", "2", "3"]
