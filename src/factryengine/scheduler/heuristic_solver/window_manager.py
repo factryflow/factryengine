b974c20227e52c6d907e86bf399238389c1c1f14
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2024-01-24 14:35:21 +0100

    revamp window manage with use structured array

--- src/factryengine/scheduler/heuristic_solver/window_manager.py
+++ src/factryengine/scheduler/heuristic_solver/window_manager.py
@@ -6,56 +6,33 @@ from ...models.resource import Resource
 class WindowManager:
     def __init__(self, resources: list[Resource]):
         self.resources = resources
-        self.resource_windows_dict = self.create_resource_windows_dict()
+        self.resource_windows_dict = self._create_resource_windows_dict()
 
-    def create_resource_windows_dict(self) -> dict[int, np.ndarray]:
-        """
-        Creates a dictionary mapping resource IDs to numpy arrays representing windows.
-        """
-        return {
-            resource.id: self.windows_to_numpy(resource.available_windows)
-            for resource in self.resources
-        }
-
-    def get_task_resource_windows(
+    def get_task_resource_windows_dict(
         self, task_resource_ids: list[int], task_earliest_start: int
-    ) -> list[np.ndarray]:
+    ) -> dict[int, np.ndarray]:
         """
         Returns the resource windows for the resource ids.
         The windows are trimmed to the min_task_start.
         """
-        return [
-            trimmed_window
-            for resource_id in task_resource_ids
-            if (
-                trimmed_window := self.trim_window(
-                    window=self.resource_windows_dict[resource_id],
-                    trim_interval=(0, task_earliest_start),
-                )
-            ).size
-            > 0
-        ]
+        trimmed_windows_dict = {}
 
-    # def windows_to_numpy(self, windows: list[tuple[int, int]]) -> np.ndarray:
-    #     """
-    #     Converts a list of windows to a numpy array.
-    #     """
-    #     arr = np.array(windows)
-    #     return np.concatenate([arr, np.diff(arr), np.zeros((arr.shape[0], 1))], axis=1)
+        # Loop over each resource ID
+        for resource_id in task_resource_ids:
+            # Get the window for the current resource ID
+            resource_windows = self.resource_windows_dict[resource_id]
 
-    def windows_to_numpy(self, windows: list[tuple[int, int]]) -> np.ndarray:
-        """
-        Converts a list of windows to a numpy array.
-        """
-        arr = np.array(windows)
-        result = np.zeros(
-            arr.shape[0],
-            dtype=[("start", int), ("end", int), ("duration", int), ("is_split", int)],
-        )
-        result["start"], result["end"] = arr[:, 0], arr[:, 1]
-        result["duration"] = np.diff(arr, axis=1).flatten()
-        result["is_split"] = 0
-        return result
+            # Trim the window to the task's earliest start time
+            trimmed_window = self._trim_window(
+                window=resource_windows,
+                trim_interval=(0, task_earliest_start),
+            )
+
+            # If the trimmed window is not empty, add it to the dictionary
+            if trimmed_window.size > 0:
+                trimmed_windows_dict[resource_id] = trimmed_window
+
+        return trimmed_windows_dict
 
     def update_resource_windows(
         self, allocated_resource_windows_dict: dict[int, list[tuple[int, int]]]
@@ -65,11 +42,49 @@ class WindowManager:
         """
         for resource_id, trim_interval in allocated_resource_windows_dict.items():
             window = self.resource_windows_dict[resource_id]
-            self.resource_windows_dict[resource_id] = self.trim_window(
+            self.resource_windows_dict[resource_id] = self._trim_window(
                 window, trim_interval
             )
 
-    def trim_window(
+    def _create_resource_windows_dict(self) -> dict[int, np.ndarray]:
+        """
+        Creates a dictionary mapping resource IDs to numpy arrays representing windows.
+        """
+        return {
+            resource.id: self._windows_to_numpy(resource.available_windows)
+            for resource in self.resources
+        }
+
+    def _windows_to_numpy(self, windows: list[tuple[int, int]]) -> np.ndarray:
+        """
+        Converts a list of windows to a numpy array.
+        """
+        # Convert the list of windows to a numpy array
+        arr = np.array(windows)
+
+        # Define the dtype for the structured array
+        dtype = [
+            ("start", np.float32),
+            ("end", np.float32),
+            ("duration", np.float32),
+            ("is_split", np.int32),
+        ]
+
+        # Create an empty structured array with the specified dtype
+        result = np.zeros(arr.shape[0], dtype=dtype)
+
+        # Fill the 'start' and 'end' fields with the first and second columns of 'arr', respectively
+        result["start"], result["end"] = arr[:, 0], arr[:, 1]
+
+        # Calculate the duration of each window and fill the 'duration' field
+        result["duration"] = np.diff(arr, axis=1).flatten()
+
+        # Fill the 'is_split' field with zeros
+        result["is_split"] = 0
+
+        return result
+
+    def _trim_window(
         self, window: np.ndarray, trim_interval: tuple[int, int]
     ) -> np.ndarray:
         """
@@ -78,27 +93,27 @@ class WindowManager:
         window = window.copy()
         trim_start, trim_end = trim_interval
 
-        start_idx = np.searchsorted(window[:, 1], trim_start, side="right")
-        end_idx = np.searchsorted(window[:, 0], trim_end, side="left")
+        start_idx = np.searchsorted(window["end"], trim_start, side="right")
+        end_idx = np.searchsorted(window["start"], trim_end, side="left")
 
         if start_idx == end_idx:
             return window
 
-        overlap_windows = window[start_idx:end_idx]
-        mask_end = overlap_windows[:, 1] <= trim_end
-        mask_start = overlap_windows[:, 0] >= trim_start
+        overlap_intervals = window[start_idx:end_idx]
+        mask_end = overlap_intervals["end"] <= trim_end
+        mask_start = overlap_intervals["start"] >= trim_start
         mask_delete = np.logical_and(mask_end, mask_start)
         mask_between = np.logical_and(
-            overlap_windows[:, 0] < trim_start, overlap_windows[:, 1] > trim_end
+            overlap_intervals["start"] < trim_start, overlap_intervals["end"] > trim_end
         )
 
         slopes = self._calculate_slopes(
-            overlap_windows
+            overlap_intervals
         )  # Compute slopes for all overlap_windows
 
         window = self._handle_mask_between(
             window,
-            overlap_windows,
+            overlap_intervals,
             mask_between,
             slopes,
             trim_start,
@@ -106,21 +121,23 @@ class WindowManager:
             start_idx,
             end_idx,
         )
-        window = self._handle_mask_end(
-            window, overlap_windows, mask_end, trim_start, end_idx
+        window = self._handle_mask(
+            window, overlap_intervals, mask_end, trim_start, "end", end_idx
         )
-        window = self._handle_mask_start(window, overlap_windows, mask_start, trim_end)
+        window = self._handle_mask(
+            window, overlap_intervals, mask_start, trim_end, "start"
+        )
         window = self._delete_overlapped_windows(
             window, mask_delete, start_idx, end_idx
         )
 
         return window
 
-    def _calculate_slopes(self, windows: np.ndarray) -> np.ndarray:
+    def _calculate_slopes(self, window: np.ndarray) -> np.ndarray:
         """
-        Calculates the slopes for the given windows.
+        Calculates the slopes for the given intervals.
         """
-        return (windows[:, 2] - windows[:, 3]) / (windows[:, 1] - windows[:, 0])
+        return window["duration"] / (window["end"] - window["start"])
 
     def _handle_mask_between(
         self,
@@ -139,16 +156,16 @@ class WindowManager:
         if np.any(mask_between):
             slopes_between = slopes[mask_between]
             overlap_windows = np.concatenate([overlap_windows, overlap_windows])
-            overlap_windows[0, 1] = trim_start
-            overlap_windows[0, 2] = (
-                overlap_windows[0, 1] - overlap_windows[0, 0]
+            overlap_windows[0]["end"] = trim_start
+            overlap_windows[0]["duration"] = (
+                overlap_windows[0]["end"] - overlap_windows[0]["start"]
             ) * slopes_between
 
-            overlap_windows[1, 0] = trim_end
-            overlap_windows[1, 2] = (
-                overlap_windows[1, 1] - overlap_windows[1, 0]
+            overlap_windows[1]["end"] = trim_end
+            overlap_windows[1]["duration"] = (
+                overlap_windows[1]["end"] - overlap_windows[1]["start"]
             ) * slopes_between
-            overlap_windows[1, 3] = -1
+            overlap_windows[1]["is_split"] = -1
 
             return np.concatenate(
                 (windows[:start_idx], overlap_windows, windows[end_idx:])
@@ -156,43 +173,30 @@ class WindowManager:
 
         return windows
 
-    def _handle_mask_end(
+    def _handle_mask(
         self,
         windows: np.ndarray,
         overlap_windows: np.ndarray,
-        mask_end: np.ndarray,
-        trim_start: int,
-        end_idx: int,
+        mask: np.ndarray,
+        trim_value: int,
+        field: str,
+        end_idx: int = None,
     ) -> np.ndarray:
         """
-        Handles the case where mask_end is True.
+        Handles the case where mask is True.
         """
-        if np.any(mask_end):
-            overlap_windows[mask_end, 1] = trim_start
-            overlap_windows[mask_end, 2] = (
-                overlap_windows[mask_end, 1] - overlap_windows[mask_end, 0]
+        if np.any(mask):
+            overlap_windows[mask][field] = trim_value
+            overlap_windows[mask]["duration"] = (
+                overlap_windows[mask]["end"] - overlap_windows[mask]["start"]
             )
-            end_idx_temp = min(end_idx, windows.shape[0] - 1)  # handle out of bounds
-            windows[end_idx_temp, 3] = -1
-
-        return windows
-
-    def _handle_mask_start(
-        self,
-        windows: np.ndarray,
-        overlap_windows: np.ndarray,
-        mask_start: np.ndarray,
-        trim_end: int,
-    ) -> np.ndarray:
-        """
-        Handles the case where mask_start is True.
-        """
-        if np.any(mask_start):
-            overlap_windows[mask_start, 0] = trim_end
-            overlap_windows[mask_start, 2] = (
-                overlap_windows[mask_start, 1] - overlap_windows[mask_start, 0]
-            )
-            overlap_windows[mask_start, 3] = -1
+            if field == "end" and end_idx is not None:
+                end_idx_temp = min(
+                    end_idx, windows.shape[0] - 1
+                )  # handle out of bounds
+                windows[end_idx_temp]["is_split"] = -1
+            else:
+                overlap_windows[mask]["is_split"] = -1
 
         return windows
 
