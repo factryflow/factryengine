a292f264a2a9770131848c4b6bc14ce50168e200
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2024-02-05 14:07:49 +0100

    ignore numpy warnings

--- src/factryengine/__init__.py
+++ src/factryengine/__init__.py
@@ -1,2 +1,9 @@
+import warnings
+
+import numpy as np
+
 from .models import Assignment, Resource, ResourceGroup, Task
 from .scheduler.core import Scheduler
+
+# Ignore numpy's UserWarning
+warnings.filterwarnings("ignore", category=UserWarning, module="numpy.*")
